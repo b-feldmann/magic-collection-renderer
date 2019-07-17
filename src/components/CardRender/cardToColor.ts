b97cd24d3febab52eb505a979752e09fad70d740
Benjamin Feldmann <benjamin-feldmann@web.de>
2019-07-17 20:30:41 +0200

    Better p-Mana

--- src/components/CardRender/cardToColor.ts
+++ src/components/CardRender/cardToColor.ts
@@ -24,22 +24,62 @@ const cardToColor = (
       switch (cost) {
         case 'w':
         case 'W':
+        case 'wp':
+        case 'Wp':
+        case 'wP':
+        case 'WP':
+        case 'pw':
+        case 'pW':
+        case 'Pw':
+        case 'PW':
           setColor(ColorType.White);
-          break;
-        case 'b':
-        case 'B':
-          setColor(ColorType.Black);
           break;
         case 'u':
         case 'U':
+        case 'up':
+        case 'Up':
+        case 'uP':
+        case 'UP':
+        case 'pu':
+        case 'pU':
+        case 'Pu':
+        case 'PU':
           setColor(ColorType.Blue);
+          break;
+        case 'b':
+        case 'B':
+        case 'bp':
+        case 'Bp':
+        case 'bP':
+        case 'BP':
+        case 'pb':
+        case 'pB':
+        case 'Pb':
+        case 'PB':
+          setColor(ColorType.Black);
           break;
         case 'r':
         case 'R':
+        case 'rp':
+        case 'Rp':
+        case 'rP':
+        case 'RP':
+        case 'pr':
+        case 'pR':
+        case 'Pr':
+        case 'PR':
           setColor(ColorType.Red);
           break;
         case 'g':
         case 'G':
+        case 'gp':
+        case 'Gp':
+        case 'gP':
+        case 'GP':
+        case 'pg':
+        case 'pG':
+        case 'Pg':
+        case 'PG':
           setColor(ColorType.Green);
           break;
       }
